3c4d1acd67a0a9951e38eb2cab3a4f203ce99c33
Laymansdecker <brendenrschmitz@gmail.com>
2024-12-09 12:03:03 +1100

    Zig Zag enemy made
    -created an enemy variant that moves downward in a zigzag line

--- Assets/Scripts/EnemyZigZag.cs
+++ Assets/Scripts/EnemyZigZag.cs
@@ -2,32 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyZigZag : EnemyController
+public class EnemyZigZag : EnemyController  //INHERITANCE
 {
-    //private float horizontalSpeed = 10f;
-    [SerializeField] private float xSpawn;
-    [SerializeField] private float xPosition;
-    [SerializeField] private float xtargetPosition;
-    private float zigZagLimit = 2f;
-    static float t = 0f;
+    [SerializeField] private float spawnPos;
+    [SerializeField] private float targetPos;
+    [SerializeField] private float leftPos;
+    [SerializeField] private float rightPos;
+    [SerializeField] private float moveLimit = 3.0f;
+
+
 
     private void Awake()
     {
-        xSpawn = transform.position.x;                           //sets the original spawn x position
-        xtargetPosition = xSpawn - zigZagLimit;
-    }
-    public override void MoveDown()
-    {
-        xPosition = transform.position.x;
-        speed = 10;                                                 //updates the speed of the object
-        transform.Translate(Vector3.back * Time.deltaTime * speed); //moves the enemy down the screen
-        if(xPosition > xtargetPosition)
-        {
-            transform.position = new Vector3(Mathf.Lerp(xSpawn, xtargetPosition, t), transform.position.y, transform.position.z);
-            //transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
-            t = 0.1f * Time.deltaTime;
-        }
+        spawnPos = transform.position.x;    //sets the spawn z position
+        leftPos = spawnPos - moveLimit;     //sets the movement to the left
+        rightPos = spawnPos + moveLimit;    //sets the movement to the right
+        targetPos = leftPos;                //sets the initial target for the enemy to move towards
 
+
+
+    }
+    public override void MoveDown() //POLYMORPHISM
+    {
+        // we begin to move the enemy towards the targetPos, which in this case is leftPos
+        if (transform.position.x > targetPos && targetPos == leftPos)
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, -speed * Time.deltaTime);
+        }
+        //once we hit the targetPost, we change the targetPos to the rightPos
+        else if (transform.position.x <= targetPos && targetPos == leftPos)
+        {
+            targetPos = rightPos;
+        }
+        //and begin to move the enemy along the axis back towards the rightPost
+        else if (transform.position.x < targetPos && targetPos == rightPos)
+        {
+            transform.Translate(speed * Time.deltaTime, 0, -speed * Time.deltaTime);
+        }
+        //Once we hit the rightPos, we reset the target position to the leftPos and repeat the loop
+        else if (transform.position.x >= targetPos && targetPos == rightPos)
+        {
+            targetPos = leftPos;
+        }
     }
 
 
